eb2275a1c63b7909b649413af4059d4195c84ad2
Matthew <matthewwebb65@yahoo.com>
2015-11-23 21:05:02 -0600

    Removed counter because permission must be granted for write access

--- scripts/counter.js
+++
@@ -1,8 +0,0 @@
-if (window.File && window.FileReader && window.FileList && window.Blob) {
-    var f = new File([""], "count.txt")
-    var reader = new FileReader();
-    reader.readAsText(new File([""], "count.txt"));
-    alert("contents: " + reader.result);
-} else {
-    alert("File APIs missing!");
-}
